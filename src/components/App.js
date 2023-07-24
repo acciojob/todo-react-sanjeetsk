cfe5aa8c08b22db02d3a98ee7933097529e9a8e7
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-24 13:23:29 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
 
-import React from "react";
+import React,{ useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAddItems = () => {
+    const newItem = {
+      name: document.getElementById("ItemName").value,
+    };
+
+    const updatedTodoList = [...todoList, newItem];
+
+    setTodoList(updatedTodoList);
+
+    //clear input fields
+
+    document.getElementById('ItemName').value='';
+    
+  }
+
+  const handleRemoveItem = (itemName) => {
+    const updatedTodoList = todoList.filter((item)=>item.name !== itemName);
+    setTodoList(updatedTodoList);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+        <div id="add-item">
+          <input type="text" id="ItemName" />
+          <button onClick={handleAddItems}>Add Todo</button>
+        </div>
+
+        {
+          todoList && todoList.map((item, index) => (
+            <div key={index}>
+              <span className="itemName">{item.name}</span>
+              <button onClick={() => handleRemoveItem(item.name)}>Delete</button>
+            </div>
+          ))
+        }
+
     </div>
   )
 }
